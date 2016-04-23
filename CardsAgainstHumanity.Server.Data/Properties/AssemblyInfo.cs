d798b98d1263a3f9ad40630c3093407acbbdc576
Robert Clabough <rmcn96@gmail.com>
2016-04-23 17:04:34 -0500

    Initial Commit

--- CardsAgainstHumanity.Server.Data/Properties/AssemblyInfo.cs
+++ CardsAgainstHumanity.Server.Data/Properties/AssemblyInfo.cs
@@ -1,20 +1,26 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CardsAgainstHumanity")]
+[assembly: AssemblyTitle("CardsAgainstHumanity.Server.Data")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CardsAgainstHumanity")]
+[assembly: AssemblyProduct("CardsAgainstHumanity.Server.Data")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1e8d48b6-32a3-448f-ac22-1da563f03c80")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ CardsAgainstHumanity.Server.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CardsAgainstHumanity.Server.Web.MvcApplication" Language="C#" %>
