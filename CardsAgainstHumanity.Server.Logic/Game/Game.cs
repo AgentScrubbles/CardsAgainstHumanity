2973dedbf9c10f919fbf8f58ba8a83b2667b5987
Robert Clabough <rmcn96@gmail.com>
2016-04-23 19:23:43 -0500

    Game logic

--- CardsAgainstHumanity.Server.Logic/Game/Game.cs
+++ CardsAgainstHumanity.Server.Logic/Game/Game.cs
@@ -1,12 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CardsAgainstHumanity.Server.Data;
+using CardsAgainstHumanity.Shared.Extensions;
 
 namespace CardsAgainstHumanity.Server.Logic.Game
 {
     public class Game
     {
+        private readonly ICardService _cardService;
 
-        public IEnumerable<Guid> AvailableWhiteCards { get; set; } 
-        public IEnumerable<Guid> AvailableBlackCards { get; set; }
+        public Game(ICardService cardService)
+        {
+            _cardService = cardService;
+            GameId = Guid.NewGuid();
+        }
+        public Guid GameId { get; }
+
+        public IList<Guid> AvailableWhiteCards { get; private set; } 
+        public IList<Guid> AvailableBlackCards { get; private set; }
+        public bool IsRunning { get; private set; }
+        public List<Guid> Players { get; private set; } 
+
+        public List<Round> Rounds { get; private set; }  
+
+        public async Task Setup()
+        {
+            AvailableWhiteCards = (await _cardService.GetAllWhiteCards()).Select(k => k.Key).Shuffle().ToList();
+            AvailableBlackCards = (await _cardService.GetAllBlackCards()).Select(k => k.Key).Shuffle().ToList();
+            Rounds = new List<Round>();
+            Players = new List<Guid>();
+            IsRunning = true;
+        }
+
+        public void AddPlayer(Guid playerId)
+        {
+            //eh, a player can be added again, won't do anything
+            if (Players.All(k => k != playerId))
+            {
+                Players.Add(playerId);
+            }
+        }
+
+        public void RemovePlayer(Guid playerId)
+        {
+            if (Players.Contains(playerId))
+            {
+                Players.Remove(playerId);
+            }
+        }
+
+        public Round CreateRound()
+        {
+            var blackCardId = AvailableBlackCards.FirstOrDefault();
+            AvailableBlackCards.Remove(blackCardId);
+            var round = new Round(_cardService, blackCardId, Players);
+            Rounds.Add(round);
+            if (AvailableBlackCards.Count == 0)
+            {
+                IsRunning = false;
+            }
+            return round;
+        }
+
+        public IDictionary<Guid, int> GetScores()
+        {
+            return Rounds.GroupBy(k => k.Winner).ToDictionary(k => k.Key, v => v.Count());
+        } 
     }
 }
---
+++ CardsAgainstHumanity.Server.Logic/Game/GameService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Concurrent;
+using CardsAgainstHumanity.Server.Data;
+using CardsAgainstHumanity.Shared.Extensions;
+
+namespace CardsAgainstHumanity.Server.Logic.Game
+{
+
+    public interface IGameServiceFactory
+    {
+        
+    }
+
+    public class GameServiceFactory
+    {
+        private readonly ICardService _cardService;
+
+        public GameServiceFactory(ICardService cardService)
+        {
+            _cardService = cardService;
+        }
+
+        public static GameService Instance;
+
+
+        public GameService Get()
+        {
+            lock (typeof (IGameServiceFactory))
+            {
+                return Instance ?? (Instance = new GameService(_cardService));
+            }
+        }
+    }
+
+    public class GameService
+    {
+        private readonly ICardService _cardService;
+        private static readonly ConcurrentDictionary<Guid, Game> ActiveGames = new ConcurrentDictionary<Guid, Game>();
+
+        public GameService(ICardService cardService)
+        {
+            _cardService = cardService;
+        }
+
+        public Guid CreateGame()
+        {
+            var gameId = Guid.NewGuid();
+            ActiveGames[gameId] = new Game(_cardService);
+            return gameId;
+        }
+
+        public Game GetGame(Guid gameId)
+        {
+            return ActiveGames.Get(gameId);
+        }
+
+        public void EndGame(Guid gameId)
+        {
+            Game game;
+            ActiveGames.TryRemove(gameId, out game);
+        }
+    }
+}
